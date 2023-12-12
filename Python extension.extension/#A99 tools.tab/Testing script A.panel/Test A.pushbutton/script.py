6c94be5bce57a89e73bff54dc68cef17c8215c25
imlerith01 <dv.jakub2@gmail.com>
2023-12-12 12:14:54 +0100

    1.28

--- Python extension.extension/#A99 tools.tab/Testing script A.panel/Test A.pushbutton/script.py
+++ Python extension.extension/#A99 tools.tab/Testing script A.panel/Test A.pushbutton/script.py
@@ -1,64 +1,33 @@
-import clr
-clr.AddReference('RevitAPI')
-clr.AddReference('RevitAPIUI')
-from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB import FilteredElementCollector, FamilyInstance, Family, Transaction
 from Autodesk.Revit.UI.Selection import ObjectType
-from pyrevit import forms, script
+from pyrevit import revit, DB, script, forms
 
-doc = __revit__.ActiveUIDocument.Document
-uidoc = __revit__.ActiveUIDocument
-
-# Function to select rooms
-def select_rooms():
-    selected_refs = uidoc.Selection.PickObjects(ObjectType.Element, "Select rooms")
-    rooms = [doc.GetElement(ref.ElementId) for ref in selected_refs if doc.GetElement(ref.ElementId).Category.Name == 'Rooms']
-    return rooms
-
-# Function to get all floor type names
-def get_floor_type_names():
-    floor_types = FilteredElementCollector(doc).OfClass(FloorType).ToElements()
-    floor_type_names = [ft.Name for ft in floor_types]
-    return floor_type_names
-
-# Function to get floor type by name
-def get_floor_type_by_name(name):
-    floor_types = FilteredElementCollector(doc).OfClass(FloorType).ToElements()
-    for ft in floor_types:
-        if ft.Name == name:
-            return ft
-    return None
-
-# Function to create floor for each room
-def create_floors(rooms, floor_type):
-    with Transaction(doc, "Create Floors") as trans:
-        trans.Start()
-        for room in rooms:
-            level_id = room.LevelId
-            boundary = room.GetBoundarySegments(SpatialElementBoundaryOptions())[0]
-            curve_loop = CurveLoop.Create([seg.GetCurve() for seg in boundary])
-            floor = Floor.Create(doc, curve_loop, floor_type.Id, level_id)
-        trans.Commit()
+# Function to get nested families
+def get_nested_families(family):
+    nested_families = []
+    for family_symbol_id in family.GetFamilySymbolIds():
+        nested_family_symbol = doc.GetElement(family_symbol_id)
+        # Additional checks can be added here
+        nested_families.append(nested_family_symbol.Family.Name)
+    return nested_families
 
 # Main script execution
-# Step 1: Select rooms
-rooms = select_rooms()
-if not rooms:
-    script.exit()
+doc = revit.doc
+uidoc = revit.uidoc
 
-# Step 2: Choose floor type
-floor_type_names = get_floor_type_names()
+# Prompt user to select a family instance
+try:
+    selected_ref = uidoc.Selection.PickObject(ObjectType.Element, "Select a family instance")
+    selected_instance = doc.GetElement(selected_ref.ElementId)
+    selected_family = selected_instance.Symbol.Family
 
-if not floor_type_names:
-    forms.alert("No floor types found.")
-    script.exit()
+    # Get nested families
+    nested_families = get_nested_families(selected_family)
 
-selected_floor_type_name = forms.SelectFromList.show(sorted(floor_type_names), "Select a Floor Type")
-
-if not selected_floor_type_name:
-    script.exit()
-
-selected_floor_type = get_floor_type_by_name(selected_floor_type_name)
-
-# Step 3: Create floors
-create_floors(rooms, selected_floor_type)
-forms.alert('Floors created successfully.')
+    # Display results
+    if nested_families:
+        forms.alert('Nested Families:\n' + '\n'.join(nested_families), exitscript=True)
+    else:
+        forms.alert('No nested families found.', exitscript=True)
+except Exception as e:
+    forms.alert('Error or operation cancelled by user: {}'.format(str(e)), exitscript=True)
